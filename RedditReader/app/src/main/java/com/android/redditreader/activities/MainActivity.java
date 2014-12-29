25951de8b1009b687467ee16e9499f698f5b4a4d
Manas Bajaj <manas.bajaj94@gmail.com>
2014-12-29 10:53:52 +0530

    Added new java packages: activities, adapters, async_tasks, fragments, models and utils. Moved existing files to appropriate packages.

--- RedditReader/app/src/main/java/com/android/redditreader/activities/MainActivity.java
+++ RedditReader/app/src/main/java/com/android/redditreader/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package com.android.redditreader;
+package com.android.redditreader.activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.android.redditreader.R;
 
 
 public class MainActivity extends ActionBarActivity {
