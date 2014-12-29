5243a8290ef89c642696963262f090546ad22649
Manas Bajaj <manas.bajaj94@gmail.com>
2014-12-29 11:05:26 +0530

    Added new fragment: NavigationDrawerFragment and its corresponding layout. This fragment represents the body of the navigation drawer.

---
+++ RedditReader/app/src/main/java/com/android/redditreader/fragments/NavigationDrawerFragment.java
@@ -0,0 +1,28 @@
+package com.android.redditreader.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.redditreader.R;
+
+
+public class NavigationDrawerFragment extends Fragment {
+
+    public NavigationDrawerFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
+        return view;
+    }
+
+
+}
