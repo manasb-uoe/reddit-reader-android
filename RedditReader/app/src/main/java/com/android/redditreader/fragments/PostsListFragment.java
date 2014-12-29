fdb03998f26d2b9252ab0dc80340863767fb52e4
Manas Bajaj <manas.bajaj94@gmail.com>
2014-12-29 13:43:19 +0530

    Updated fragment: PostsListFragment, to use GetPostsTask in order to load front page posts when the fragment is first created.

--- RedditReader/app/src/main/java/com/android/redditreader/fragments/PostsListFragment.java
+++ RedditReader/app/src/main/java/com/android/redditreader/fragments/PostsListFragment.java
@@ -1,8 +1,9 @@
 package com.android.redditreader.fragments;
 
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,11 +11,18 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
 import com.android.redditreader.R;
+import com.android.redditreader.adapters.PostsAdapter;
+import com.android.redditreader.background_tasks.GetPostsTask;
+import com.android.redditreader.models.Post;
+import com.android.redditreader.utils.Helpers;
+
+import java.util.ArrayList;
 
 public class PostsListFragment extends Fragment {
 
     private RecyclerView postsRecyclerView;
     private ProgressBar postsProgressBar;
+    private PostsAdapter postsAdapter;
 
     public PostsListFragment() {
         // Required empty public constructor
@@ -26,12 +34,24 @@ public class PostsListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_posts_list, container, false);
         findViews(view);
 
+        // set up posts recycler view
+        postsAdapter = new PostsAdapter(getActivity(), new ArrayList<Post>());
+        postsRecyclerView.setAdapter(postsAdapter);
+        postsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // load posts into the recycler view using defaults
+        new GetPostsTask(Helpers.getCurrentSubredditLink(), postsAdapter, postsRecyclerView, postsProgressBar).execute();
+
         return view;
     }
 
     private void findViews(View view) {
         postsRecyclerView = (RecyclerView) view.findViewById(R.id.posts_recycler_view);
         postsProgressBar = (ProgressBar) view.findViewById(R.id.posts_progress_bar);
+    }
+
+    public void refreshPosts() {
+        new GetPostsTask(Helpers.getCurrentSubredditLink(), postsAdapter, postsRecyclerView, postsProgressBar).execute();
     }
 
 }
