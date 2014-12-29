cd986202924767bcb7e173907480e3c8d7c1883e
Manas Bajaj <manas.bajaj94@gmail.com>
2014-12-29 14:35:00 +0530

    Updated MainActivity to handle sorting submenu item clicks.

--- RedditReader/app/src/main/java/com/android/redditreader/activities/MainActivity.java
+++ RedditReader/app/src/main/java/com/android/redditreader/activities/MainActivity.java
@@ -1,15 +1,17 @@
 package com.android.redditreader.activities;
 
+import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.android.redditreader.R;
+import com.android.redditreader.fragments.PostsListFragment;
+import com.android.redditreader.utils.Globals;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -19,6 +21,7 @@ public class MainActivity extends ActionBarActivity {
     private ActionBar actionBar;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
+    private PostsListFragment postsListFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +29,14 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         findViews();
 
+        postsListFragment = (PostsListFragment) getSupportFragmentManager().findFragmentById(R.id.posts_list_fragment);
+
         // enable UP button so that it can be used by ActionBarDrawerToggle
         actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeButtonEnabled(true);
+
+        updateActionBarText();
 
         setUpNavigationDrawer();
     }
@@ -50,12 +57,28 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
-                // TODO update actionbar title
+                updateActionBarText();
             }
         };
 
         drawerLayout.setDrawerListener(drawerToggle);
         drawerToggle.syncState();
+    }
+
+    private void updateActionBarText() {
+        if (!Globals.CURRENT_SUBREDDIT.equals(Globals.DEFAULT_SUBREDDIT)) {
+            actionBar.setTitle("/r/" + Globals.CURRENT_SUBREDDIT);
+        }
+        else {
+            actionBar.setTitle(Globals.CURRENT_SUBREDDIT);
+        }
+
+        if (Globals.CURRENT_TIME != null) {
+            actionBar.setSubtitle(Globals.CURRENT_SORT + ": " + Globals.CURRENT_TIME);
+        }
+        else {
+            actionBar.setSubtitle(Globals.CURRENT_SORT);
+        }
     }
 
     @Override
@@ -72,6 +95,34 @@ public class MainActivity extends ActionBarActivity {
         if (drawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
+        if (id == R.id.action_sort_hot || id == R.id.action_sort_new || id == R.id.action_sort_rising) {
+            Globals.CURRENT_SORT = item.getTitle().toString();
+            Globals.CURRENT_TIME = null;
+
+            updateActionBarText();
+
+            postsListFragment.refreshPosts();
+        }
+        if (id == R.id.action_sort_top_hour || id == R.id.action_sort_top_day || id == R.id.action_sort_top_week
+                || id == R.id.action_sort_top_month || id == R.id.action_sort_top_year || id == R.id.action_sort_top_all) {
+            Globals.CURRENT_SORT = "Top";
+            Globals.CURRENT_TIME = item.getTitle().toString();
+
+            updateActionBarText();
+
+            postsListFragment.refreshPosts();
+        }
+        if (id == R.id.action_sort_controversial_hour || id == R.id.action_sort_controversial_day
+                || id == R.id.action_sort_controversial_week || id == R.id.action_sort_controversial_month ||
+                id == R.id.action_sort_controversial_year || id == R.id.action_sort_controversial_all) {
+            Globals.CURRENT_SORT = "Controversial";
+            Globals.CURRENT_TIME = item.getTitle().toString();
+
+            updateActionBarText();
+
+            postsListFragment.refreshPosts();
+        }
+
         return super.onOptionsItemSelected(item);
     }
 }
