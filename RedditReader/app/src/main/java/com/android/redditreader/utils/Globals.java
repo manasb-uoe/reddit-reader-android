d32a4f80d22c3b279357bc66635522fef875db33
Manas Bajaj <manas.bajaj94@gmail.com>
2014-12-29 11:21:12 +0530

    Added new utility class: Globals, which includes commonly used static fields.

---
+++ RedditReader/app/src/main/java/com/android/redditreader/utils/Globals.java
@@ -0,0 +1,16 @@
+package com.android.redditreader.utils;
+
+/**
+ * Created by Manas on 29-12-2014.
+ */
+public class Globals {
+
+    public static final String DEFAULT_SUBREDDIT = "Front Page";
+    public static final String DEFAULT_SORT = "Hot";
+    public static final int DEFAULT_LIMIT = 15;
+
+    public static String CURRENT_SUBREDDIT = DEFAULT_SUBREDDIT;
+    public static String CURRENT_SORT = DEFAULT_SORT;
+    public static String CURRENT_TIME = null;
+    public static String CURRENT_POSTS_AFTER = null;
+}
