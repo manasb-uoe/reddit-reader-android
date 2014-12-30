a495ebc4e1c365c7b0cb0be26e198181cb9aa75d
Manas Bajaj <manas.bajaj94@gmail.com>
2014-12-30 20:15:56 +0530

    Added new utility class: RoundedImageView, which will be used to display images with rounded corners. Updated row_posts.xml accordingly.

---
+++ RedditReader/app/src/main/java/com/android/redditreader/utils/RoundedImageView.java
@@ -0,0 +1,38 @@
+package com.android.redditreader.utils;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by Manas on 30-12-2014.
+ */
+public class RoundedImageView extends ImageView {
+
+    private final float radius = 8.0f;
+
+    public RoundedImageView(Context context) {
+        super(context);
+    }
+
+    public RoundedImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RoundedImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //float radius = 36.0f;
+        Path clipPath = new Path();
+        RectF rect = new RectF(0, 0, this.getWidth(), this.getHeight());
+        clipPath.addRoundRect(rect, radius, radius, Path.Direction.CW);
+        canvas.clipPath(clipPath);
+        super.onDraw(canvas);
+    }
+}
