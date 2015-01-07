fa2a572420a1e4e07a78b27cac53ab45353f6e74
Manas Bajaj <manas.bajaj94@gmail.com>
2015-01-07 19:37:00 +0530

    Updated Subreddit model to implement Parcelable interface so that its instances can be passed between activities. Also added a new attribute: isFavourite to the model, which indicates whether a subreddit is user's favourite or not.

--- RedditReader/app/src/main/java/com/android/redditreader/models/Subreddit.java
+++ RedditReader/app/src/main/java/com/android/redditreader/models/Subreddit.java
@@ -1,19 +1,32 @@
 package com.android.redditreader.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.text.NumberFormat;
 
 /**
  * Created by Manas on 02-01-2015.
  */
-public class Subreddit {
+public class Subreddit implements Parcelable {
 
     private String name;
     private String numOfSubscribers;
     private boolean isSubscribed;
     private String description;
+    private boolean isFavourite;
 
     public Subreddit() {
         //empty constructor
+    }
+
+    // Parcel constructor
+    public Subreddit(Parcel source) {
+        this.name = source.readString();
+        this.numOfSubscribers = source.readString();
+        this.isSubscribed = source.readInt() == 1;
+        this.description = source.readString();
+        this.isFavourite = source.readInt() == 1;
     }
 
     public String getName() {
@@ -47,4 +60,39 @@ public class Subreddit {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public boolean isFavourite() {
+        return isFavourite;
+    }
+
+    public void setFavourite(boolean isFavourite) {
+        this.isFavourite = isFavourite;
+    }
+
+    // --------Parcelling--------
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(numOfSubscribers);
+        dest.writeInt(isSubscribed ? 1 : 0);
+        dest.writeString(description);
+        dest.writeInt(isFavourite ? 1 : 0);
+    }
+
+    public static final Creator<Subreddit> CREATOR = new Creator<Subreddit>() {
+        @Override
+        public Subreddit createFromParcel(Parcel source) {
+            return new Subreddit(source);
+        }
+
+        @Override
+        public Subreddit[] newArray(int size) {
+            return new Subreddit[size];
+        }
+    };
 }
